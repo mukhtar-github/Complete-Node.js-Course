0a7a54bcbef82ca2fac287526a912a639c4e6994
Mukhtar <mukhtartanimugarba@gmail.com>
2020-09-20 05:56:37 +0100

    Files updated

--- getting-started.js
+++ getting-started.js
@@ -185,22 +185,29 @@
 //And with this simple change, the Logger class will have all the functionality that is defined in the EventEmitter.
 // So, inside the Logger class, instead of using the emitter object to raise an event, we use THIS keyword. So, we can
 //directly emit or raise an event from the Logger class. Now, we no longer need the emitter object created from the
-//EventEmitter class in the logger module, so we delete it.
-
+//EventEmitter class in the logger module, so we delete it. So, in the getting-started module when requiring a logger
+//module, we get a class, so we rename it.
+//Similar to the change we made in the logger module, we no longer need the EventEmitter in this module as well. We
+//want to work directly with the logger object. So we're going to register the listener on the logger object, we 
+//move the listener after the created logger object.
+//So, to recap, if you want to raise events in your application to signal that something has happened, you need to create
+//a class that extends EventEmitter. With this, that class will have all the functionality defined in EventEmitter. And
+//you can also have additional functionality, in this case, we have the ability to log a message. And then, inside that
+//class, when ever you want to raise an event, you use THIS.emit, because THIS references the Logger class wich extends
+// EventEmitter
 
 const EventEmitter = require('events');
-const emitter = new EventEmitter();
 
+const Logger = require('./logger');
+const logger = new Logger();
 
 //Register a Listener
-emitter.on('messageLogged', (arg) => {
+logger.on('messageLogged', (arg) => {
     console.log('Listener called', arg);
 });
 
-const log = require('./logger');
-log('message');
+logger.log('message');
 
 
-
-
+// 13 - HTTP Module
 
