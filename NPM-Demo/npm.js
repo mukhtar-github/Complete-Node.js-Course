c3b38a189ed51a1d6adb92af0c5329e8d8ad6d5e
Mukhtar <mukhtartanimugarba@gmail.com>
2020-09-23 15:46:45 +0100

    Files updated

--- NPM-Demo/npm.js
+++ NPM-Demo/npm.js
@@ -7,3 +7,12 @@
 //packages to your application, you need to create a package.json file. So, as a best practice whenever you start
 //a Node project, you need to run npm-init to create package.json. The faster way to create a package.json file is
 //to supply the flag npm init --yes.
+
+
+// 3 - Installing a Node Package
+
+//In this lecture, I'm going to show you how to add a third party library, or a third party Node package to your
+//Node application. So, we're going to install a popular Javascript library called underscore.
+
+
+
