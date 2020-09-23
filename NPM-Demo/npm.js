af571ae91d7169237c584bd09c928cca86b08ae6
Mukhtar <mukhtartanimugarba@gmail.com>
2020-09-23 15:40:16 +0100

    Files updated

--- NPM-Demo/npm.js
+++ NPM-Demo/npm.js
@@ -1,3 +1,9 @@
 // 2 - Package.json
 
-//Before you add any Node packages to your application, you need to create
+//Before you add any Node packages to your application, you need to create a file that is called package.json
+//Package.json is basically a json file that includes some basic information about your application or your
+//project, such as it's name, it's version, it's authors and so on. It's basically a bunch of metadata about your
+//application. And all Node applications by standard have this package.json file. Moreover, before adding any node 
+//packages to your application, you need to create a package.json file. So, as a best practice whenever you start
+//a Node project, you need to run npm-init to create package.json. The faster way to create a package.json file is
+//to supply the flag npm init --yes.
