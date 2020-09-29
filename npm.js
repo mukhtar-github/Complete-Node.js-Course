7af6698d31bf558ff89801d2d006a72b8cfbbc4a
Mukhtar <mukhtartanimugarba@gmail.com>
2020-09-29 14:09:45 +0100

    Files updated

--- npm.js
+++ npm.js
@@ -18,10 +18,15 @@ const { contains } = require('underscore');
 
 // 4 - Using a Package
 
-//Let's create a new file in the root of this project, and call it npm.js.
-//We're going to use our require function to load the underscore module. By convention, we use
-//an underscore for referring to the underscore library. We use the require function, and as
-//the argument, we supply underscore. This is how the require function works.
+//Let's create a new file in the root of this project, and call it npm.js. We're going to use our require function to load
+//the underscore module. By convention, we use an underscore for referring to the underscore library. We use the require
+//function, and as the argument, we supply underscore. This is how the require function works. When you supply the module
+//name as an argument inside the require function, first it assumes the module is a core module. In Node, we don't have a
+//core module called underscore. So the require function thinks maybe this underscore is a file or folder in this project.
+//However, earlier in the course, you learn that in order to reference a file or folder, we use (./). So, if the argument
+//to the require function was (./underscore), then the require function would assume that we have a file called underscore.js
+//in the same folder. If not, it would assume underscore is a folder, and inside this folder we have a file called index.js.
+//But in this case, we neither have a file or folder called underscore.
 
 var _ = require('underscore');
 
