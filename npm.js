392a594ff1cae92004c0610cbd4a55a3327bf2b6
Mukhtar <mukhtartanimugarba@gmail.com>
2020-10-02 22:01:48 +0100

    File updated

--- npm.js
+++ npm.js
@@ -67,7 +67,8 @@ const { contains } = require('underscore');
 // 7 - Semantic Versioning
 
 //So earlier in this section, I mentioned the Caret character. What does this mean? Well, in order to understand this, first
-//we need to understand Semantic Versioning 
+//we need to understand Semantic Versioning. Which is also called Samver. In Semanti versioning, the version of a Node package
+//has three components. 
 
 
 
