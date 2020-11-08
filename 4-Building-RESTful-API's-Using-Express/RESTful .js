0658a906d982dc8ed92940b83766ec9d90b73800
Mukhtar <mukhtartanimugarba@gmail.com>
2020-11-08 14:39:22 +0100

    File update

--- 4-Building-RESTful-API's-Using-Express/RESTful .js
+++ 4-Building-RESTful-API's-Using-Express/RESTful .js
@@ -3,4 +3,6 @@
 //So, earlier in section 2, were we talked about Node's Module System. You learned about 'HTTP Module'. We use it to create
 //a web server that listens on 'port 3000' and responds to requests to the end-points. So the root or '/api/courses'. Now,
 //while this approach is perfectly fine, it's not ideal for building a complex application. Because in a large complex 
-//application, we might have various endpoints and
+//application, we might have various endpoints and we don't want to hard code all these 'if' statements in the function. So
+//in this section, we're going to look at 'Express' which is a fast and light weight framework forb building web applications.
+//So  next we're going to look at 'RESTful services.
