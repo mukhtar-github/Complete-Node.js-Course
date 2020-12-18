c4c6605b3195abe402574e727d658acc9acc6821
Mukhtar <mukhtartanimugarba@gmail.com>
2020-12-18 08:25:05 +0100

    File update

--- 4-Building-RESTful-API's-Using-Express/express-demo/index.js
+++ 4-Building-RESTful-API's-Using-Express/express-demo/index.js
@@ -9,7 +9,7 @@ app.get('/api/courses', (req, res) => {
     res.send([1, 2, 3]);
 });
 
-app.get('/api/courses/:id', (req, res) => {
+app.get('/api/posts/:year/:month', (req, res) => {
     res.send(req.params.id);
 });
 
