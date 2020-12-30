80ce3dc7af04d5342f3955b5444701fe8fc445d5
Mukhtar <mukhtartanimugarba@gmail.com>
2020-12-30 11:18:52 +0100

    Update files

--- 4-Building-RESTful-API's-Using-Express/RESTful.js
+++ 4-Building-RESTful-API's-Using-Express/RESTful.js
@@ -96,56 +96,56 @@
 
 // console.log('Listening on port 3000...');
 
-//So here's the code that we wrote in the section about Node core, where I introduced you to the http module. So we can see,
-//with http module we can create a web server. Here we have a callback function that takes two parameters, requests and
-//response. And with this request object, we can check the URL of the incoming request. So with this, we can define various
-//routes for our application. So if we have a request for let's say (req.url === '/api/courses'), this is how we're going
-//to respond to the client { res.write(JSON.stringify([1, 2, 3])); }. Now, while this approach certainly works, it's not
-//very maintainable, because as we define more routes for our application, we need to add more 'if' blocks in the callback
-//function. So that's when a framework comes into the picture. A framework gives our application a proper structure, so we
-//can easily add more routes, while keeping our application code maintainable. There are various frameworks out there for
-//building web applications and web servers on top of Node. The most popular one is Express. So if you head over to npmjs.com,
-//and search for express. So back in the terminal, let's create a folder and call it 'express-demo'. Let's run npm init 
-//with --yes flag inside the folder. So now, we have a package.json file and finally we can install express. In the next 
-//lecture I'm going to show you how to build your first web server using express.
+// So here's the code that we wrote in the section about Node core, where I introduced you to the http module. So we can see,
+// with http module we can create a web server. Here we have a callback function that takes two parameters, requests and
+// response. And with this request object, we can check the URL of the incoming request. So with this, we can define various
+// routes for our application. So if we have a request for let's say (req.url === '/api/courses'), this is how we're going
+// to respond to the client { res.write(JSON.stringify([1, 2, 3])); }. Now, while this approach certainly works, it's not
+// very maintainable, because as we define more routes for our application, we need to add more 'if' blocks in the callback
+// function. So that's when a framework comes into the picture. A framework gives our application a proper structure, so we
+// can easily add more routes, while keeping our application code maintainable. There are various frameworks out there for
+// building web applications and web servers on top of Node. The most popular one is Express. So if you head over to npmjs.com,
+// and search for express. So back in the terminal, let's create a folder and call it 'express-demo'. Let's run npm init 
+// with --yes flag inside the folder. So now, we have a package.json file and finally we can install express. In the next 
+// lecture I'm going to show you how to build your first web server using express.
 
 
 // 4 - Building Your First Web Server
 
-//Alright, let's add a new file and name it index.js. You could also call it app.js, it doesn't really matter. So in this 
-//file, first we want to load the express module. So we use our require function, give it the name of our module, which is
-//express. Now this returns a function, we call that express. Now we need to call this function (see index.js). As you can 
-//see, this returns an object of type express. By covention, we call this object, app. So we store the result in a constant
-//called app. So this represents our application. This app object has a bunch of useful methods. We have methods like, GET,
-//POST, PUT and DELETE. All these methods correspond with http verbs, or http methods that I told about earlier in this 
-//section. So if you want to handle an http Post request and endpoint, you would use app.post(). In this lecture we just 
-//want to use app.get(). We want to impliment a coulple of end-points that respond to an http get requests. So this method 
-//takes two arguments. The first argument is the path or the url. So here I'm going to use slash (/) to represent the root
-//of the website. Now the second argument is a callback function. This is the function that would called when we have an 
-//http 'get' request to this endpoint. So this callback function should have two arguments, Request and Respond.
-//This request object has a bunch of useful properties that gives us information about the in-coming requests. If you want
-//to learn about all these properties, its best to look at the Express Documentation, because in this course we're going use
-//only a handful of these properties. So, head over to Expressjs.com. On the top, look at the API reference, version 4. Now
-//here we can see the request object, and below you can see all the properties that are available to you. We have 'baseUrl',
-//we 'body' to read the body of the request, 'cookies', 'fresh', 'hostname', 'ip', and so on. So back to our code, when we 
-//get an http request to the root of our website, we're going to respond in 'Hello World' message.
-//So response.send('Hello World'). So this is how we define a route. We specify the path or the Url, and a callback function
-//which is also called a route handler. Now finally, we need to listen on a given port. So we call app.listen. We give it a
-//port number like 3000, and optionally we can pass a function that will be called when the application starts listening on
-//the given port. So once again we use the arrow function syntax to display something on the console. So back in the terminal,
-//'node index.js'. Okay we're listening on port 3000. Now let's switch over to Chrome and go 'localhost:3000'. So here's Our
-//'Hello World' message. Now let's define another route.
-//Once again, we're going to call 'app.get', this one is going to be /api/courses. Once again you pass a function with two
-//arguments, that is request and response, and this goes to a code block. Now in a real-world scenario, here you want to
-//get the list of courses from the database and return them. But as I told you before, in this section our focus is purely
-//on building these endpoints, we're not going to do any database work. So I'm going to simply return an array of numbers.
-//So response.send, we pass an array of 3 numbers. In the future we can replace these numbers with actual course objects.
-//So save. Now back in the terminal, we have to stop the earlier process and start it again. So, press control and C to 
-//cancel. Okay, one more time, 'node index.js'. Now back in Chrome, let's head over to '/api/courses'. Look we have an array of
-//3 numbers, beautiful. So this is what I want you to pay attention to here, in this implementation, we don't have those
-//'if (?)' blocks. We define new route, like calling 'app.get', and with this structure, as our application grows, we can
-//move some of these routes to different files. For example, we can move all the routes related to courses to a separate
-//file, like 'courses.js'. So Express gives our application a skeleton, a structure.
+// Alright, let's add a new file and name it index.js. You could also call it app.js, it doesn't really matter. So in this 
+// file, first we want to load the express module. So we use our require function, give it the name of our module, which is
+// express. Now this returns a function, we call that express. Now we need to call this function (see index.js). As you can 
+// see, this returns an object of type express. By covention, we call this object, app. So we store the result in a constant
+// called app. So this represents our application. This app object has a bunch of useful methods. We have methods like, GET,
+// POST, PUT and DELETE. All these methods correspond with http verbs, or http methods that I told about earlier in this 
+// section. So if you want to handle an http Post request and endpoint, you would use app.post(). In this lecture we just 
+// want to use app.get(). We want to impliment a coulple of end-points that respond to an http get requests. So this method 
+// takes two arguments. The first argument is the path or the url. So here I'm going to use slash (/) to represent the root
+// of the website. Now the second argument is a callback function. This is the function that would called when we have an 
+// http 'get' request to this endpoint. So this callback function should have two arguments, Request and Respond.
+// This request object has a bunch of useful properties that gives us information about the in-coming requests. If you want
+// to learn about all these properties, its best to look at the Express Documentation, because in this course we're going use
+// only a handful of these properties. So, head over to Expressjs.com. On the top, look at the API reference, version 4. Now
+// here we can see the request object, and below you can see all the properties that are available to you. We have 'baseUrl',
+// we 'body' to read the body of the request, 'cookies', 'fresh', 'hostname', 'ip', and so on. So back to our code, when we 
+// get an http request to the root of our website, we're going to respond in 'Hello World' message.
+// So response.send('Hello World'). So this is how we define a route. We specify the path or the Url, and a callback function
+// which is also called a route handler. Now finally, we need to listen on a given port. So we call app.listen. We give it a
+// port number like 3000, and optionally we can pass a function that will be called when the application starts listening on
+// the given port. So once again we use the arrow function syntax to display something on the console. So back in the terminal,
+// 'node index.js'. Okay we're listening on port 3000. Now let's switch over to Chrome and go 'localhost:3000'. So here's Our
+// 'Hello World' message. Now let's define another route.
+// Once again, we're going to call 'app.get', this one is going to be /api/courses. Once again you pass a function with two
+// arguments, that is request and response, and this goes to a code block. Now in a real-world scenario, here you want to
+// get the list of courses from the database and return them. But as I told you before, in this section our focus is purely
+// on building these endpoints, we're not going to do any database work. So I'm going to simply return an array of numbers.
+// So response.send, we pass an array of 3 numbers. In the future we can replace these numbers with actual course objects.
+// So save. Now back in the terminal, we have to stop the earlier process and start it again. So, press control and C to 
+// cancel. Okay, one more time, 'node index.js'. Now back in Chrome, let's head over to '/api/courses'. Look we have an array of
+// 3 numbers, beautiful. So this is what I want you to pay attention to here, in this implementation, we don't have those
+// 'if (?)' blocks. We define new route, like calling 'app.get', and with this structure, as our application grows, we can
+// move some of these routes to different files. For example, we can move all the routes related to courses to a separate
+// file, like 'courses.js'. So Express gives our application a skeleton, a structure.
 
 
 // 5 - Nodemon
