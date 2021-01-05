552bc0cfa16adb14a1a295a5cd4ca4959d39d884
Mukhtar <mukhtartanimugarba@gmail.com>
2021-01-05 02:00:58 +0100

    Update files

--- 4-Building-RESTful-API's-Using-Express/express-demo/index.js
+++ 4-Building-RESTful-API's-Using-Express/express-demo/index.js
@@ -19,6 +19,13 @@ app.get('/api/courses', (req, res) => {
     res.send(courses);
 });
 
+//Get a single course
+app.get('/api/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if(!course) res.status(404).send('The course with the given ID was not found');
+    res.send(course);
+});
+
 app.post('/api/courses', (req, res) => {
     if(!req.body.name || req.body.name.length < 3) {
         // 400 Bad request
@@ -30,13 +37,6 @@ app.post('/api/courses', (req, res) => {
         name: req.body.name
     };
     courses.push(courses);
-    res.send(course);
-});
-
-//Get a single course
-app.get('/api/courses/:id', (req, res) => {
-    const course = courses.find(c => c.id === parseInt(req.params.id));
-    if(!course) res.status(404).send('The course with the given ID was not found');
     res.send(course);
 });
 
