a6d0a97e2124e7df775e3fafde9f1b6bb84f2711
Mukhtar <mukhtartanimugarba@gmail.com>
2021-01-09 14:52:38 +0100

    Add a index.js file

--- 5-Express- Advanced Topics/Express- Advanced.md
+++ 5-Express- Advanced Topics/Express- Advanced.md
@@ -5,3 +5,5 @@
 In the last session, we learned how to build RESTful services using *Express*. In this section we're going to continue our journey with *Express* and look at more *advanced topics*. More specifically, we'll be looking at *Middleware*, *Configuration*, *Debugging*, *Templating Engines*, and more. So now, let's get started!
 
 ## 2- Middleware
+
+One of the core concepts in *Express* that we need to learn, is the concept of *Middleware*, or *Middleware function*. The *Middleware function* is basically a function that takes a request object, and either returns a response to the client, or passes control to another *Middleware function*. You have already seen two examples of *Middleware functions*, one is this route handler
---
+++ 5-Express- Advanced Topics/index.js
@@ -0,0 +1,84 @@
+const Joi = require('joi');
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+const courses = [
+    { id: 1, name: 'course1'},
+    { id: 2, name: 'course2'},
+    { id: 3, name: 'course3'}
+];
+
+app.get('/', (req, res) => {
+    res.send('Hello World!!!');
+});
+
+// GET all courses
+app.get('/api/courses', (req, res) => {
+    res.send(courses);
+});
+
+// GET a single course
+app.get('/api/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if(!course) return res.status(404).send('The course with the given ID was not found');
+    res.send(course);
+});
+
+// POST Endpoint
+app.post('/api/courses', (req, res) => {
+    const { errror } = validateCourse(req.body);
+    if(error)  return res.status(400).send(error.details[0].message);
+
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    };
+
+    courses.push(courses);
+    res.send(course);
+});
+
+// PUT Requests
+app.put('/api/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    
+    if(!course) return res.status(404).send('The course with the given ID was not found');
+
+    const { error } = validateCourse(req.body);
+   
+    if(error) return res.status(400).send(error.details[0].message);
+        
+    course.name = req.body.name;
+    res.send(course);
+});
+
+// DELETE Request
+app.delete('/api/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if(!course) return res.status(404).send('The course with the given ID was not found');
+
+    const index = courses.indexOf(course);
+    courses.splice(index, 1);
+
+    res.send(course);
+});
+
+// Validate Function
+function validateCourse(course) {
+    const schema = {
+        name: Joi.string().min(3).required()
+    };
+
+   return Joi.validate(course, schema);
+}
+
+// PORT
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
+
+// Testing
+app.get('/api/posts/:year/:month', (req, res) => {
+    res.send(req.query);
+});
