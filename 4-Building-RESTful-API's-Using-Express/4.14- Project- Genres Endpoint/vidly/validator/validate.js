8439971b4569653004332169d73c38ac1521cb48
Mukhtar <mukhtartanimugarba@gmail.com>
2021-01-20 04:20:19 +0100

    Add two folders and update files

---
+++ 4-Building-RESTful-API's-Using-Express/4.14- Project- Genres Endpoint/vidly/validator/validate.js
@@ -0,0 +1,10 @@
+
+function validateGenre(genre) {
+    const schema = {
+      name: Joi.string().min(3).required()
+    };
+  
+    return Joi.validate(genre, schema);
+  }
+
+module.exports = validateGenre;
