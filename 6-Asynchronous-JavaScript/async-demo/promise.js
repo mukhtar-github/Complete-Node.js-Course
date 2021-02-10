a05da3fc1bb131cd627373ca95b088ddd99aeaff
Mukhtar <mukhtartanimugarba@gmail.com>
2021-02-10 03:54:02 +0100

    Update file

--- 6-Asynchronous-JavaScript/async-demo/promise.js
+++ 6-Asynchronous-JavaScript/async-demo/promise.js
@@ -35,16 +35,22 @@ function getUser(id) {
    
 }
 
-function getRepositories(username, callback) {
-  setTimeout(() => {
-    console.log('Calling GitHub API...');
-    callback(['repo1', 'repo2', 'repo3']);
-  }, 2000);
+function getRepositories(username) {
+  return new Promise((resolve, reject) => {
+    // Kick off some async work
+    setTimeout(() => {
+      console.log('Calling GitHub API...');
+      resolve(['repo1', 'repo2', 'repo3']);
+    }, 2000);
+  });
 }
 
-function getCommits(repo, callback) {
-  setTimeout(() => {
-    console.log('Calling GitHub API...');
-    callback(['commit']);
-  }, 2000);
+function getCommits(repo) {
+  return new Promise((resolve, reject) => {
+    // Kick off some async work
+    setTimeout(() => {
+      console.log('Calling GitHub API...');
+      resolve(['commit']);
+    }, 2000);
+  });
 }
