2f28ffc3fad35b30c0b877e54c470c0d9ab3dcd4
Mukhtar <mukhtartanimugarba@gmail.com>
2021-02-16 10:15:28 +0100

    Update files

--- 6-Asynchronous-JavaScript/async-demo/promise.js
+++ 6-Asynchronous-JavaScript/async-demo/promise.js
@@ -26,9 +26,7 @@ console.log('Before');
 getUser(1)
 .then(user => getRepositories(user.gitHubUsername))
 .then(repos => getCommits(repos[0]))
-.then(commits => {
-  console.log(commits);
-});
+.then(commits => console.log('Commits', commits));
 
 //.catch(err => console.log('Error', err.message));
 
