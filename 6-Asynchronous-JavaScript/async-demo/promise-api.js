bf031d81b2fd5b64eac6a7374fc9238cec6fd64e
Mukhtar <mukhtartanimugarba@gmail.com>
2021-03-13 09:15:03 +0100

    Update files

--- 6-Asynchronous-JavaScript/async-demo/promise-api.js
+++ 6-Asynchronous-JavaScript/async-demo/promise-api.js
@@ -16,13 +16,20 @@
 
 // Running Promises in Parallel
 
-const p1 = new Promise((resolve) => {
-   setTimeout(() => {
-       console.log('Async operation 1...');
-       resolve(1);
-   }, 2000);
-});
+// const p1 = new Promise((resolve) => {
+//    setTimeout(() => {
+//        console.log('Async operation 1...');
+//        resolve(1);
+//    }, 2000);
+// });
 
+const p1 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        console.log('Async operation 1...');
+        reject(new Error('Because something failed.'));
+    }, 2000);
+ });
+ 
 const p2 = new Promise((resolve) => {
     setTimeout(() => {
         console.log('Async operation 2...');
