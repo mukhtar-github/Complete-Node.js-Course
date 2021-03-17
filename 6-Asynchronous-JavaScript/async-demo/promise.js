c98cc3ded89622e34926486cb7e399bfef570739
Mukhtar <mukhtartanimugarba@gmail.com>
2021-03-17 08:09:35 +0100

    Update files

--- 6-Asynchronous-JavaScript/async-demo/promise.js
+++ 6-Asynchronous-JavaScript/async-demo/promise.js
@@ -36,12 +36,14 @@ getUser(1)
 
 // async and wait approach
 
-function displayCommits() {
+async function displayCommits() {
   const user = await getUser(1);
   const repos = await getRepositories(user.gitHubUsername);
   const commits = await getCommits(repos[0]);
   console.log('Commits', commits);
 }
+
+displayCommits();
 
 
 console.log('After');
