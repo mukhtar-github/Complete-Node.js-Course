0b000f5877c7a429280c8d399d0a0b12d05326aa
Mukhtar <mukhtartanimugarba@gmail.com>
2021-05-25 09:32:04 +0100

    Update files

--- 7-CRUD Operations Using Mongoose/mongo-demo/index.js
+++ 7-CRUD Operations Using Mongoose/mongo-demo/index.js
@@ -29,7 +29,7 @@ async function createCourse() {
 async function getCourses() {
     const courses = await Course
     // .find({ author: 'Mukhtar', isPublished: true })
-    .find( { prize: { $: } })
+    .find( { prize: { $gt: 10 } })
     .limit(10)
     .sort( { name: 1, })
     .select( {name: 1, tags: 1});
