c38d0198ab878b13fda3d1461c88f8fabe4d213a
Mukhtar <mukhtartanimugarba@gmail.com>
2021-06-08 14:28:04 +0100

    Update file

--- 7-CRUD Operations Using Mongoose/mongo-demo/exercise1.js
+++ 7-CRUD Operations Using Mongoose/mongo-demo/exercise1.js
@@ -3,3 +3,12 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/mongo-exercises')
 .then(() => console.log(' Connected to MongoDB...'))
 .catch(err => console.error('Coulld not connect to MongoDB..', err));
+
+const courseSchema = new mongoose.Schema({
+    name: String,
+    author: String,
+    tags: [ String ],
+    date: Date,
+    isPublished: Boolean,
+    price: Number
+});
