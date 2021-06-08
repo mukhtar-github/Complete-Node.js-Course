c4cdbb8f63295b09a0128edbe5c35544ceb28af0
Mukhtar <mukhtartanimugarba@gmail.com>
2021-06-08 15:35:25 +0100

    Update file

--- 7-CRUD Operations Using Mongoose/mongo-demo/exercise1.js
+++ 7-CRUD Operations Using Mongoose/mongo-demo/exercise1.js
@@ -15,3 +15,16 @@ const courseSchema = new mongoose.Schema({
 
 const Course = mongoose.model('Course', courseSchema);
 
+async function getCourses() {
+    return await Course
+    .find({ isPublished: true, tags: 'backend' })
+    .sort({ name: 1 })
+    .select({ name: 1, author: 1 });
+}
+
+async function run() {
+    const courses = await getCourses();
+    console.log(courses);
+}
+  
+run();
