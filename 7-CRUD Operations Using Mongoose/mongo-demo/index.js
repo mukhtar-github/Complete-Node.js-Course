dc9b442a4dc336bd9617780b6d254a5c54d1ef04
Mukhtar <mukhtartanimugarba@gmail.com>
2021-06-25 07:01:23 +0100

    Update file

--- 7-CRUD Operations Using Mongoose/mongo-demo/index.js
+++ 7-CRUD Operations Using Mongoose/mongo-demo/index.js
@@ -4,27 +4,27 @@ mongoose.connect('mongodb://localhost/playground')
 .then(() => console.log('Connected to MongoDB...'))
 .catch(err => console.error('Could not connect to MongoDB...', err));
 
-const courseSchema = new mongoose.Schema({
-    name: String,
-    author: String,
-    tags: [ String ],
-    date: { type: Date, default: Date.now, },
-    isPublished: Boolean
-});
+// const courseSchema = new mongoose.Schema({
+//     name: String,
+//     author: String,
+//     tags: [ String ],
+//     date: { type: Date, default: Date.now, },
+//     isPublished: Boolean
+// });
 
-const Course = mongoose.model('Course', courseSchema);
+// const Course = mongoose.model('Course', courseSchema);
 
-async function createCourse() {
-    const course = new Course({
-        name: 'Angular Course',
-        author: 'Mukhtar',
-        tags: ['angular', 'frontend'],
-        isPublished: true
-    });
+// async function createCourse() {
+//     const course = new Course({
+//         name: 'Angular Course',
+//         author: 'Mukhtar',
+//         tags: ['angular', 'frontend'],
+//         isPublished: true
+//     });
     
-    const result = await course.save();
-    console.log(result);
-}
+//     const result = await course.save();
+//     console.log(result);
+// }
 
 // async function getCourses() {
 //     const courses = await Course
@@ -51,18 +51,18 @@ async function createCourse() {
 
 // getCourses();
 
-async function getCourses() {
-    const pageNumber = 2;
-    const pageSize = 10;
-    // /api/courses?pageNumber=2&pageSize=10
-    const courses = await Course
-        .find({ author: 'Mukhtar', isPublished: true })
-        .skip((pageNumber - 1) * pageSize)
-        .limit(pageSize)
-        .sort({ name: 1, })
-        .count();
-    console.log(courses);
-}
+// async function getCourses() {
+//     const pageNumber = 2;
+//     const pageSize = 10;
+//     // /api/courses?pageNumber=2&pageSize=10
+//     const courses = await Course
+//         .find({ author: 'Mukhtar', isPublished: true })
+//         .skip((pageNumber - 1) * pageSize)
+//         .limit(pageSize)
+//         .sort({ name: 1, })
+//         .count();
+//     console.log(courses);
+// }
 
 // async function updateCourse(id) {
 //     const course = await Course.findById(id);
