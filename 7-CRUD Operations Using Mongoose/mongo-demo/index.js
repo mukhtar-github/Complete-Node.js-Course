f6a9bca46138d1b8acd4dcd2aa8a2832760917b6
Mukhtar <mukhtartanimugarba@gmail.com>
2021-06-25 07:18:42 +0100

    Update file

--- 7-CRUD Operations Using Mongoose/mongo-demo/index.js
+++ 7-CRUD Operations Using Mongoose/mongo-demo/index.js
@@ -4,15 +4,15 @@ mongoose.connect('mongodb://localhost/playground')
 .then(() => console.log('Connected to MongoDB...'))
 .catch(err => console.error('Could not connect to MongoDB...', err));
 
-// const courseSchema = new mongoose.Schema({
-//     name: String,
-//     author: String,
-//     tags: [ String ],
-//     date: { type: Date, default: Date.now, },
-//     isPublished: Boolean
-// });
+const courseSchema = new mongoose.Schema({
+    name: String,
+    author: String,
+    tags: [ String ],
+    date: { type: Date, default: Date.now, },
+    isPublished: Boolean
+});
 
-// const Course = mongoose.model('Course', courseSchema);
+const Course = mongoose.model('Course', courseSchema);
 
 // async function createCourse() {
 //     const course = new Course({
